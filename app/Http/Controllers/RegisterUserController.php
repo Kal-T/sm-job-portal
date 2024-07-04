d316d20b3a0435e9199a2a85841c3c797067f7ec
Thiha Htet Zaw <thihahtetzaw7991@gmail.com>
2024-07-04 19:02:27 +0630

    Add Authentication & Authorization

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterUserController extends Controller
 {
@@ -11,6 +14,17 @@ class RegisterUserController extends Controller
     }
 
     public function store(){
-        return view('auth.register');
+        $attrs = request()->validate([
+            'first_name'=>['required'],
+            'last_name'=>['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(6), 'confirmed'],
+        ]);
+
+        $user = User::create($attrs);
+
+        Auth::login($user);
+
+        return redirect('/jobs');
     }
 }
